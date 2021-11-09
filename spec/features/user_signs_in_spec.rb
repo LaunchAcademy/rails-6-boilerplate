3565b993c70f65d85652fc3d09cc58035f02c4ad
Jonathan Stern <jonathan.r.stern@gmail.com>
2021-11-09 16:38:43 -0500

    User Auth RSpec tests halfway complete

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature "Sign in" do
+  scenario 'User signs in' do
+    # visit '/'
+
+    # click_on 'Sign In'
+
+    # fill_in 'Email', with: "test@email.com"
+    # fill_in 'Password', with: "password123"
+  
+    # expect(page).to have_content('Signed in successfully.')
+    # expect(page).to have_content('Sign Out')
+  end
+end
