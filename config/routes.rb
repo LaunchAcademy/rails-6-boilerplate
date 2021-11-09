babbbf5fdc4a81090114f5b44a2976e917ce86c7
saulleee <26151666+saulleee@users.noreply.github.com>
2021-11-09 17:19:00 -0500

    WIP on the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "species#index"
   
-  resources :species, only: [:index]
-  resources :breeds, only: [:index]
+  resources :species, only: [:index, :show] do
+    resources :breeds, only: [:index]
+  end
 end
