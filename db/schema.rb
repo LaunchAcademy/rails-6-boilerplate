11963d6a567a5c90d0bf3e7dd76bbbb4442afdd0
Raffi Sahagian <raffisahagian@raffis-mbp.myfiosgateway.com>
2021-11-10 14:09:25 -0500

    Finished Devise rspec tests

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,21 @@ ActiveRecord::Schema.define(version: 2021_11_09_181951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.string "description", null: false
+    t.bigint "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_comments_on_question_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
